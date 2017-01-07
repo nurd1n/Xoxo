109f2c26e3db36c752f190d42249a2e7eb8f67cc
Nurdianto <nurdiantopntr@gmail.com>
2017-01-07 09:53:51 +0700

    update python

--- install_windows.sh
+++ install_windows.sh
@@ -6,16 +6,16 @@ mkdir video_awal
 mkdir video_akhir
 mkdir record_tv
 
-# upgrade pip & setuptools
-pip install --upgrade pip setuptools
+# upgrade pip, setuptools, wget, curl
+python -m pip install --upgrade pip setuptools wget curl
 # jika pakai pip, gagal maka jalankan : easy_install --upgrade pip setuptools
 
 # install youtube-dl
-pip install --upgrade youtube-dl
+python -m pip install --upgrade youtube-dl
 # jika pakai pip, gagal maka jalankan : easy_install --upgrade youtube-dl
 
 # install youtube-upload (optional)
-#pip install --upgrade google-api-python-client progressbar2
+#python -m pip install --upgrade google-api-python-client progressbar2
 #curl -L https://github.com/tokland/youtube-upload/archive/master.zip -o master.zip
 #unzip master.zip
 #cd youtube-upload-master
