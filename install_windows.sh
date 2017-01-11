eef3bb7633d7c4a1092cb7c0af02726ff06fd307
Nurdianto <nurdiantopntr@gmail.com>
2017-01-11 07:36:55 +0700

    update

--- install_windows.sh
+++ install_windows.sh
@@ -10,12 +10,12 @@ mkdir -p text_to_speech/en
 
 # upgrade pip, setuptools, wget, curl, youtube-dl, gTTS
 unzip -q bin/wget.zip -d bin
-command -V pip >/dev/null 2>&1 || { echo -e >&2 "$(bin/wget -q --no-check-certificate "https://bootstrap.pypa.io/get-pip.py" && python get-pip.py && rm -f get-pip.py)"; sleep 1000; }
+command -V pip >/dev/null 2>&1 || { echo -e >&2 "$(bin/wget -q --no-check-certificate "https://bootstrap.pypa.io/get-pip.py" && python get-pip.py && rm -f get-pip.py)"; sleep 1; }
 python -m pip install --upgrade pip setuptools wget curl youtube-dl gTTS
-command -V wget >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade wget)"; sleep 1000; }
-command -V curl >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade curl)"; sleep 1000; }
-command -V youtube-dl >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade youtube-dl)"; sleep 1000; }
-command -V gtts-cli.py >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade gTTS)"; sleep 1000; }
+command -V wget >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade wget)"; sleep 1; }
+command -V curl >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade curl)"; sleep 1; }
+command -V youtube-dl >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade youtube-dl)"; sleep 1; }
+command -V gtts-cli.py >/dev/null 2>&1 || { echo -e >&2 "$(easy_install --upgrade gTTS)"; sleep 1; }
 
 # install youtube-upload (optional)
 #python -m pip install --upgrade google-api-python-client progressbar2
