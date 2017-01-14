0dc4f24e168ea0b74ffa8dcd97cdeb4617d8ad46
Nurdianto <nurdiantopntr@gmail.com>
2017-01-14 09:00:40 +0700

    update 1.0.7

--- xoxo.sh
+++ xoxo.sh
@@ -12,13 +12,12 @@ command -V gtts-cli.py >/dev/null 2>&1 || { echo -e >&2 "-----------------------
 bin/wget.exe -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
 # check versi
-if [ "$(bin/wget.exe -q --no-check-certificate https://raw.githubusercontent.com/nurd1n/Xoxo/master/bin/xoxoversion -O - | grep '1.0.6')" == "1.0.6" ]; then
+if [ "$(bin/wget.exe -q --no-check-certificate https://raw.githubusercontent.com/nurd1n/Xoxo/master/bin/xoxoversion -O - | grep '1.0.7')" == "1.0.7" ]; then
 
 clear
-ffplay 'bin/audio/xoxo.mp3' -nodisp -autoexit -loglevel panic -af "atempo=0.85,volume=2"
 echo -n -e "\e[1m\e[93m$(date)\e[0m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~                          \e[1m\e[96mXOXO Version 1.0.6\e[0m                         ~
+~                          \e[1m\e[96mXOXO Version 1.0.7\e[0m                         ~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        \e[1m\e[92mHai, youtuber dan spammer, silahkan pilih menu di bawah :\e[0m
 ----------------------------------------------------------~~~~~~~~~~~~~~
@@ -106,10 +105,9 @@ fi
 else
 
 clear
-ffplay 'bin/audio/xoxo.mp3' -nodisp -autoexit -loglevel panic -af "atempo=0.85,volume=2"
 echo -n -e "\e[1m\e[93m$(date)\e[0m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~                          \e[1m\e[96mXOXO Version 1.0.6\e[0m                         ~
+~                          \e[1m\e[96mXOXO Version 1.0.7\e[0m                         ~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        \e[1m\e[92mHai, youtuber dan spammer, silahkan pilih menu di bawah :\e[0m
 ----------------------------------------------------------~~~~~~~~~~~~~~
@@ -193,13 +191,12 @@ command -V gtts-cli.py >/dev/null 2>&1 || { echo -e >&2 "-----------------------
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
 # check versi
-if [ "$(wget -q --no-check-certificate https://raw.githubusercontent.com/nurd1n/Xoxo/master/bin/xoxoversion -O - | grep '1.0.6')" == "1.0.6" ]; then
+if [ "$(wget -q --no-check-certificate https://raw.githubusercontent.com/nurd1n/Xoxo/master/bin/xoxoversion -O - | grep '1.0.7')" == "1.0.7" ]; then
 
 clear
-ffplay 'bin/audio/xoxo.mp3' -nodisp -autoexit -loglevel panic -af "atempo=0.85,volume=2"
 echo -n -e "\e[1m\e[93m$(date)\e[0m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~                          \e[1m\e[96mXOXO Version 1.0.6\e[0m                         ~
+~                          \e[1m\e[96mXOXO Version 1.0.7\e[0m                         ~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        \e[1m\e[92mHai, youtuber dan spammer, silahkan pilih menu di bawah :\e[0m
 ----------------------------------------------------------~~~~~~~~~~~~~~
@@ -222,15 +219,15 @@ echo -n -e "\e[1m\e[93m$(date)\e[0m
 read delete_option
 
 if [ "$delete_option" == "1" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxoplay.sh && echo \"chmod 755 /tmp/deletexoxoplay.sh\" | bash - ;done < bin/xoxoplay.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxoplay.sh && echo \"chmod 755 /tmp/deletexoxoplay.sh\" | bash - ;done < bin/xoxoplay.exe" | bash -
 source /tmp/deletexoxoplay.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "2" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxodownload.sh && echo \"chmod 755 /tmp/deletexoxodownload.sh\" | bash - ;done < bin/xoxodownload.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxodownload.sh && echo \"chmod 755 /tmp/deletexoxodownload.sh\" | bash - ;done < bin/xoxodownload.exe" | bash -
 source /tmp/deletexoxodownload.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "3" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxomp3.sh && echo \"chmod 755 /tmp/deletexoxomp3.sh\" | bash - ;done < bin/xoxomp3.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxomp3.sh && echo \"chmod 755 /tmp/deletexoxomp3.sh\" | bash - ;done < bin/xoxomp3.exe" | bash -
 source /tmp/deletexoxomp3.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "4" ]; then
@@ -238,19 +235,19 @@ source /tmp/deletexoxomp3.sh && rm -f /tmp/delete*
                  \e[1m\e[93mMaaf saat ini, menu belum tersedia\e[0m" ; sleep 15 ; exit
 
  elif [ "$delete_option" == "5" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxolive.sh && echo \"chmod 755 /tmp/deletexoxolive.sh\" | bash - ;done < bin/xoxolive.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxolive.sh && echo \"chmod 755 /tmp/deletexoxolive.sh\" | bash - ;done < bin/xoxolive.exe" | bash -
 source /tmp/deletexoxolive.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "6" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxobcv.sh && echo \"chmod 755 /tmp/deletexoxobcv.sh\" | bash - ;done < bin/xoxobcv.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxobcv.sh && echo \"chmod 755 /tmp/deletexoxobcv.sh\" | bash - ;done < bin/xoxobcv.exe" | bash -
 source /tmp/deletexoxobcv.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "7" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxoplaytv.sh && echo \"chmod 755 /tmp/deletexoxoplaytv.sh\" | bash - ;done < bin/xoxoplaytv.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxoplaytv.sh && echo \"chmod 755 /tmp/deletexoxoplaytv.sh\" | bash - ;done < bin/xoxoplaytv.exe" | bash -
 source /tmp/deletexoxoplaytv.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "8" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxorecordtv.sh && echo \"chmod 755 /tmp/deletexoxorecordtv.sh\" | bash - ;done < bin/xoxorecordtv.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxorecordtv.sh && echo \"chmod 755 /tmp/deletexoxorecordtv.sh\" | bash - ;done < bin/xoxorecordtv.exe" | bash -
 source /tmp/deletexoxorecordtv.sh && rm -f /tmp/delete*
 				 
  elif [ "$delete_option" == "9" ]; then
@@ -288,10 +285,9 @@ fi
 else
 
 clear
-ffplay 'bin/audio/xoxo.mp3' -nodisp -autoexit -loglevel panic -af "atempo=0.85,volume=2"
 echo -n -e "\e[1m\e[93m$(date)\e[0m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~                          \e[1m\e[96mXOXO Version 1.0.6\e[0m                         ~
+~                          \e[1m\e[96mXOXO Version 1.0.7\e[0m                         ~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        \e[1m\e[92mHai, youtuber dan spammer, silahkan pilih menu di bawah :\e[0m
 ----------------------------------------------------------~~~~~~~~~~~~~~
@@ -314,15 +310,15 @@ echo -n -e "\e[1m\e[93m$(date)\e[0m
 read delete_option
 
 if [ "$delete_option" == "1" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxoplay.sh && echo \"chmod 755 /tmp/deletexoxoplay.sh\" | bash - ;done < bin/xoxoplay.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxoplay.sh && echo \"chmod 755 /tmp/deletexoxoplay.sh\" | bash - ;done < bin/xoxoplay.exe" | bash -
 source /tmp/deletexoxoplay.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "2" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxodownload.sh && echo \"chmod 755 /tmp/deletexoxodownload.sh\" | bash - ;done < bin/xoxodownload.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxodownload.sh && echo \"chmod 755 /tmp/deletexoxodownload.sh\" | bash - ;done < bin/xoxodownload.exe" | bash -
 source /tmp/deletexoxodownload.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "3" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxomp3.sh && echo \"chmod 755 /tmp/deletexoxomp3.sh\" | bash - ;done < bin/xoxomp3.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxomp3.sh && echo \"chmod 755 /tmp/deletexoxomp3.sh\" | bash - ;done < bin/xoxomp3.exe" | bash -
 source /tmp/deletexoxomp3.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "4" ]; then
@@ -330,19 +326,19 @@ source /tmp/deletexoxomp3.sh && rm -f /tmp/delete*
                  \e[1m\e[93mMaaf saat ini, menu belum tersedia\e[0m" ; sleep 15 ; exit
 
  elif [ "$delete_option" == "5" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxolive.sh && echo \"chmod 755 /tmp/deletexoxolive.sh\" | bash - ;done < bin/xoxolive.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxolive.sh && echo \"chmod 755 /tmp/deletexoxolive.sh\" | bash - ;done < bin/xoxolive.exe" | bash -
 source /tmp/deletexoxolive.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "6" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxobcv.sh && echo \"chmod 755 /tmp/deletexoxobcv.sh\" | bash - ;done < bin/xoxobcv.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxobcv.sh && echo \"chmod 755 /tmp/deletexoxobcv.sh\" | bash - ;done < bin/xoxobcv.exe" | bash -
 source /tmp/deletexoxobcv.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "7" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxoplaytv.sh && echo \"chmod 755 /tmp/deletexoxoplaytv.sh\" | bash - ;done < bin/xoxoplaytv.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxoplaytv.sh && echo \"chmod 755 /tmp/deletexoxoplaytv.sh\" | bash - ;done < bin/xoxoplaytv.exe" | bash -
 source /tmp/deletexoxoplaytv.sh && rm -f /tmp/delete*
 
  elif [ "$delete_option" == "8" ]; then
-echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg|./ffmpeg|g' > /tmp/deletexoxorecordtv.sh && echo \"chmod 755 /tmp/deletexoxorecordtv.sh\" | bash - ;done < bin/xoxorecordtv.exe" | bash -
+echo "while read f1; do echo \"\$f1\" | xxd -r -p | sed -e 's/\\r\$//' -e 's|ffmpeg -y|./ffmpeg -y|g' > /tmp/deletexoxorecordtv.sh && echo \"chmod 755 /tmp/deletexoxorecordtv.sh\" | bash - ;done < bin/xoxorecordtv.exe" | bash -
 source /tmp/deletexoxorecordtv.sh && rm -f /tmp/delete*
 				 
  elif [ "$delete_option" == "9" ]; then
